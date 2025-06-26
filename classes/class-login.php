19ebeedadf6da5de9460e630ce9776799591889f
MuhamamdTariq <112855375+MuhamamdTariq@users.noreply.github.com>
2025-06-26 06:38:27 +0500

    Made the login page functional

--- classes/class-login.php
+++ classes/class-login.php
@@ -3,20 +3,28 @@
 if(!class_exists('Custom_Login')){
     class Custom_Login{
         private $recaptcha_keys;
+        private static $defaults;
 
         public function __construct(){
             $this->recaptcha_keys = require __DIR__ . '/class-config.php';
             //add_action('login_init', array($this, 'load_scripts'));
             add_action('login_enqueue_scripts', array($this, 'load_scripts'));
-            add_action('login_form', array($this, 'add_recaptcha_field'));
+            add_action('login_form', array($this, 'add_recaptcha_field'), 10);
+            add_filter('authenticate', array($this, 'authenticate_user'), 20, 3);
         }
 
 
         public function load_scripts(){
         wp_enqueue_script('google-recaptcha', 'https://www.google.com/recaptcha/api.js', array());
         wp_enqueue_style('custom-login-style',  Wp_Custom_Url . '/css/custom.css', array(), Wp_Custom_Version);
-        
-        $css = "
+        $this->defaults();
+
+                
+    }
+
+        public function defaults(){
+        if(!isset(self::$defaults) && !(self::$defaults instanceof Custom_Login)){
+            self::$defaults = "
             /* Custom login styles */
             .login h1 a {
                 background-image: url('" . Wp_Custom_Url . "images/hopreneur.jpg');
@@ -25,16 +33,19 @@ if(!class_exists('Custom_Login')){
                 height: 100px;
                 display: block;
                 background-color:black;
+                clip-path: circle(40% at center);
             }
 
             .g-recaptcha {
                 margin-top: 20px;
             }
             ";
-        file_put_contents(Wp_Custom_Drive . 'css/custom.css', $css, FILE_APPEND);
 
-                
-    }
+        file_put_contents(Wp_Custom_Drive . 'css/custom.css', self::$defaults, FILE_APPEND);
+
+        }
+
+        }
 
         public function add_recaptcha_field(){
             $site_key = esc_attr($this->recaptcha_keys['site_key']);
@@ -43,10 +54,40 @@ if(!class_exists('Custom_Login')){
 
 
         }
-                
+
+        public function authenticate_user($user, $username, $password){
+            if(isset($_POST['g-recaptcha-response'])){
+                $recaptcha_response = sanitize_text_field($_POST['g-recaptcha-response']);
+
+                $response = wp_remote_post('https://google.com/recaptcha/api/siteverify', [
+                    'body' => [
+                        'secret' => $this->recaptcha_keys['secret_key'],
+                        'response' => $recaptcha_response,
+                        'remoteip' => $_SERVER['REMOTE_ADDR'],
+                    ]
+                    ]);
+
+                $response_body = wp_remote_retrieve_body($response);
+                $result = json_decode($response_body, true);
+                if(empty($result['success'])){
+                    return new Wp_Error('recaptcha failed', __('ReCaptcha Failed', 'custom-login'));
+                }
+
+            }
+            else{
+                return new WP_Error('recaptcha_missing', __('Please complete the reCAPTCHA.', 'custom-login'));
+            }
+
+             if (empty($username) || empty($password)) {
+               return new WP_Error('empty_credentials', __('Username or password cannot be empty.', 'custom-login'));
+             }
+
+            return wp_authenticate_username_password(null, $username, $password);
+        }
             
-        
-    }
+            
+
+        }
 
     
 }
